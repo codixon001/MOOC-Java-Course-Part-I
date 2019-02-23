9171b708315a4e78d1ded81762f7e2d7ecd0c2b1
codixon001 <47377288+codixon001@users.noreply.github.com>
2019-02-23 11:42:28 -0800

    Create Exercise7_Multiplication.java
    This program is the same as Exercise 6, but this time the user defined int variables are multiplied.

---
+++ Exercise7_Multiplication.java
@@ -0,0 +1,15 @@
+public class Multiplication {
+ 
+    public static void main(String[] args) {
+ 
+        int a = 23;
+        int b = 12;
+ 
+        // Program your solution here. Remember to use variables a and b!
+        int multResult = a * b;
+        
+        String toPrint = a + " * " + b + " = " + multResult;
+        System.out.println(toPrint);
+    }
+ 
+}
