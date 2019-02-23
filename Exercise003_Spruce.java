612a64a57a2b29edb71ef74d9cee411c1b55f571
codixon001 <47377288+codixon001@users.noreply.github.com>
2019-02-23 12:26:58 -0800

    Update Exercise003_Spruce.java

--- Exercise003_Spruce.java
+++ Exercise003_Spruce.java
@@ -1,3 +1,6 @@
+// A program that prints a "spruce" tree of asterisks. Teaches the coder how "println" works 
+// and how character spacing will affect printing output.
+
 public class Spruce {
  
     public static void main(String[] args) {
