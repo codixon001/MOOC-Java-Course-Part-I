f45ab77ba92d9d64d8bd057a495f91c8b7cef6d7
codixon001 <47377288+codixon001@users.noreply.github.com>
2019-02-23 14:39:24 -0800

    Create Exercise050_SeparatingFirstCharacters.java

---
+++ Exercise050_SeparatingFirstCharacters.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+ 
+public class FirstCharacters {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        
+        System.out.print("Type your name: ");
+        String name = reader.nextLine();
+        
+        if (name.length() < 3) {
+            System.out.println("");
+        } else {
+            char character1 = name.charAt(0);
+            char character2 = name.charAt(1);
+            char character3 = name.charAt(2);
+            System.out.println("1. character: " + character1);
+            System.out.println("2. character: " + character2);
+            System.out.println("3. character: " + character3);
+        }
+    }
+}
