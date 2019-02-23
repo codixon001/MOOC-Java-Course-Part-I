70b87f859f362250f398360804468711487a63a3
codixon001 <47377288+codixon001@users.noreply.github.com>
2019-02-23 13:13:51 -0800

    Create Exercise018_GradesAndPoints.java

---
+++ Exercise018_GradesAndPoints.java
@@ -0,0 +1,30 @@
+// This program asks the user to enter in a point value.
+// Using if, else-if, and else conditional statements, the program prints the appropriate "grade",
+// after evaluating the point value.
+
+import java.util.Scanner;
+ 
+public class GradesAndPoints {
+ 
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+ 
+        System.out.print("Type the points [0-60]: ");
+        int points = Integer.parseInt(reader.nextLine());
+ 
+        System.out.println("");
+        if (points < 30) {
+            System.out.println("Grade: failed");
+        } else if (points < 35) {
+            System.out.println("Grade: 1");
+        } else if (points < 40) {
+            System.out.println("Grade: 2");
+        } else if (points < 45) {
+            System.out.println("Grade: 3");
+        } else if (points < 50) {
+            System.out.println("Grade: 4");
+        } else {
+            System.out.println("Grade: 5");
+        }
+    }
+}
