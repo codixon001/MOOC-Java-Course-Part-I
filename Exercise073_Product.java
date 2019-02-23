af4a1d94a53ed3ae8d075661513df44160215939
codixon001 <47377288+codixon001@users.noreply.github.com>
2019-02-23 15:07:53 -0800

    Create Exercise073_Product.java

---
+++ Exercise073_Product.java
@@ -0,0 +1,19 @@
+/**
+ *
+ * @author Curtis
+ */
+public class Product {
+    private String name;
+    private double price;
+    private int amount;
+    
+    public Product(String nameAtStart, double priceAtStart, int amountAtStart) {
+        this.name = nameAtStart;
+        this.price = priceAtStart;
+        this.amount = amountAtStart;
+    } 
+    
+    public void printProduct() {
+        System.out.println(this.name + ", Price: " + this.price + ", Amount: " + this.amount);
+    }
+}
