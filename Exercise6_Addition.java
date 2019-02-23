f1b985196f8dfa631b2ea0c8e3f5fb47398db913
codixon001 <47377288+codixon001@users.noreply.github.com>
2019-02-23 11:40:59 -0800

    Create Exercise6_Addition.java
    This program adds two user defined int variables together. Also shows the coder string and value concatenation.

---
+++ Exercise6_Addition.java
@@ -0,0 +1,14 @@
+public class Addition {
+ 
+    public static void main(String[] args) {
+ 
+        int a = 873625;
+        int b = 134251;
+        int result = a + b; // Fix this
+ 
+        String toPrint = a + " + " + b + " = " + result;
+        System.out.println(toPrint);
+ 
+    }
+ 
+}
